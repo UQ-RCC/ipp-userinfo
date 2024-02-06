6568c292b5a487a018db7552aba4820741b88834
Nishanthi Dasanayaka <n.dasanayaka@uq.edu.au>
2024-02-06 15:35:06 +1000

    [UPDATED]- Configparser updated

--- userinfo/config.py
+++ userinfo/config.py
@@ -5,12 +5,13 @@ import errno
 config = configparser.SafeConfigParser()
 config.read(["conf/ippuserinfo.conf", os.environ.get("IPP_USERINFO_CONFIG", ""), "/etc/ipp/userinfo/conf/ippuserinfo.conf"])
 
-def get(section, option, default = None, required=False):
+def get(section, option, default = None, os_env=True, required=False):
     """
     Reads config optoin from the given section, returning default if not found
     """
+    cp_vars = os.environ if os_env else None
     try:
-        return config.get(section, option).strip()
+        return config.get(section, option, vars=cp_vars).strip()
     except:
         if required: 
             raise Exception(f"option {option} is required in section {section}")
